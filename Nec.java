a7bb1c61cd7049e0f23abdab1bbefabcbe8c9f87
Anusha <anushaavula80@gmail.com>
2024-11-25 12:17:32 +0530

    jj

---
+++ Nec.java
@@ -0,0 +1,6 @@
+package cseA;
+public interface Nec
+{
+public void Knowledge();
+}
+
---
+++ Students72.java
@@ -0,0 +1,11 @@
+package cseA;
+public class Students72 
+{
+public void attitude()
+{
+System.out.println("All are very clever,but coding skills are verylow");
+}
+}
+
+
+
